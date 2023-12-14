bef137d1d5176f366970aed4a24e5e2a8e489c04
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2023-12-13 19:57:22 -0800

    Download Tailwind npm and test fetch request in js. Main goal is create new branch for JS collaboration

--- assets/horoscope.js
+++ assets/horoscope.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 var dob = document.getElementById('#button').value();
 const url = 'https://horoscope-astrology.p.rapidapi.com/horoscope?day=year&sunsign=libra';
@@ -27,4 +28,14 @@ function getApi() {
 }
 
 getApi();
+=======
+// // Define the API URL
+// const authString = btoa("7f88606d-c79d-422b-a358-9d9896a63cb4:59fcada79e1e26ba088c1a0cb7846f9eb88d9db6850eeb7bde662fdda941d5909f56df4682e6eb9ca0adbca65f153cc4038c44f7a5b6272c6eb1eb2c1e73954696f6b716a9f67badb014ae7e56f81d778a42f7d36fd969f9a5dd647b08d4fb2d9e5f162d4ae056fc2f0dbf9a261a196f");
+// let apiKey= "7f88606d-c79d-422b-a358-9d9896a63cb4";
+// let apiSecret = "59fcada79e1e26ba088c1a0cb7846f9eb88d9db6850eeb7bde662fdda941d5909f56df4682e6eb9ca0adbca65f153cc4038c44f7a5b6272c6eb1eb2c1e73954696f6b716a9f67badb014ae7e56f81d778a42f7d36fd969f9a5dd647b08d4fb2d9e5f162d4ae056fc2f0dbf9a261a196f";
+// let apiUrl = 'https://www.myapp.com';
+
+// Select the element you want to add an event listener to
+// const myButton = document.getElementById('myButton');
+>>>>>>> Stashed changes
 
