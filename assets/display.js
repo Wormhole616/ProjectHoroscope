35527433df8b0629cd3220c93ac331e86366a849
garrett lieb <liebg08@gmail.com>
2023-12-18 14:38:53 -0800

    chinese zodiac api call & disp. ~2/3 done @ 2.36pm

--- assets/display.js
+++ assets/display.js
@@ -13,5 +13,38 @@ var aboutSection =  document.getElementById ('sign');
 var singDescription = document.createElement('p');
 singDescription.textContent = horoscope.about;
  aboutSection.appendChild(singDescription);
+
+
+
+
+
+ var horoscope2 = JSON.parse(localStorage.getItem('sign2'));
+console.log(horoscope2);
+
+
+var displaysection2 = document.getElementById('display2');
+var nameTitle2 = document.createElement('h1');
+console.log(nameTitle2);
+nameTitle2.textContent = horoscope2.name;
+displaysection2.appendChild(nameTitle2);
+var aboutSection2 =  document.getElementById ('sign2');
+var singDescription2 = document.createElement('p');
+singDescription2.textContent = horoscope2.about;
+ aboutSection2.appendChild(singDescription2); 
+
+
+
+
+// // career
+// data[0].career
+// // love
+// data[0].love
+// // lucky number
+// data[0].lucky_number
+// // lucky color
+// data[0].lucky_color
+
+
+
  
  
