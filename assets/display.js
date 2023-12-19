4e61ed8c84ecc102e139390207f056893b314117
Gustavo Duque <gustavo.duque616@gmail.com>
2023-12-18 18:24:28 -0800

    final design for accordion

--- assets/display.js
+++ assets/display.js
@@ -1,17 +1,30 @@
 
-
 var horoscope = JSON.parse(localStorage.getItem('sign'));
 console.log(horoscope);
 
 
-var displaysection = document.getElementById('display');
-var nameTitle = document.createElement('h1');
-console.log(nameTitle);
-nameTitle.textContent = horoscope.name;
-displaysection.appendChild(nameTitle);
+var displaysection = document.getElementById('title');
+// var titleDisplay = document.getElementById('title');
+var nameTitle = document.createElement('h2');
+var  strenghtSection = document.getElementById ('strong');
+var  attribute = document.createElement('p');
 var aboutSection =  document.getElementById ('sign');
 var singDescription = document.createElement('p');
+var weakSection = document.getElementById ('weaknesses');
+var weakness = document.createElement('p');
+var loveSection = document.getElementById ('love');
+var love = document.createElement('p');
+console.log(nameTitle);
 singDescription.textContent = horoscope.about;
  aboutSection.appendChild(singDescription);
- 
- 
+nameTitle.textContent = "Your sign is " + horoscope.name;
+attribute.textContent = horoscope.strengths;
+strenghtSection.appendChild(attribute);
+displaysection.appendChild(nameTitle);
+singDescription.textContent = horoscope.about;
+ aboutSection.appendChild(singDescription);
+weakness.textContent = horoscope.weaknesses;
+weakSection.appendChild(weakness);
+love.textContent = horoscope.love;
+loveSection.appendChild(love);
+
